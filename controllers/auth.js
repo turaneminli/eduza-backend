9b9515aedb6c3cf6dd13c4be20ddddc11d014c57
turaneminli <eminli.turan@gmail.com>
2022-11-08 21:27:46 +0400

    Add JWT to login (with bug in incorrect password)

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const { validationResult } = require("express-validator");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const serverError500 = require("../utils/serverError");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "../.env" });
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -29,5 +31,42 @@ exports.signup = (req, res, next) => {
     .then((result) => {
       res.status(201).json({ message: "User created!", userId: result._id });
     })
-    .catch((err) => serverError500);
+    .catch((err) => serverError500(err));
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("A user with this email could not be found");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      bcrypt
+        .compare(password, user.password)
+        .then((isEqual) => {
+          if (!isEqual) {
+            const error = new Error("Wrong password");
+            error.statusCode = 401;
+            throw error;
+          }
+          const token = jwt.sign(
+            {
+              email: loadedUser.email,
+              userId: loadedUser._id.toString(),
+            },
+            `${process.env.JWT_TOKEN_SECRET}`,
+            { expiresIn: "1h" }
+          );
+          res
+            .status(200)
+            .json({ token: token, userId: loadedUser._id.toString() });
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => serverError500(err));
 };
